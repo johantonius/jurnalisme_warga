70b7a78595a657202ebd57e58731dc5cd36b65da
Nadhira Noor <nadhira103@gmail.com>
2019-04-10 02:04:50 +0700

    add GET and PUT di UserController

--- src/main/java/com/project/jw/controller/UserController.java
+++ src/main/java/com/project/jw/controller/UserController.java
@@ -6,13 +6,11 @@ import com.project.jw.repository.UserRepository;
 import com.project.jw.services.MapValidationErrorService;
 import com.project.jw.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -29,12 +27,18 @@ public class UserController {
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
 
+    @GetMapping
+    public Iterable <User> findAll(){
+        return repository.findAll();
+    }
+
     @PostMapping("/registerMedia")
     public ResponseEntity<?> registerMedia(@Valid @RequestBody User user, BindingResult result){
 //                userValidator.validate(user, result);
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if (result.hasErrors()) return null;
 
+        user.setStatus(false);
         user.setRole(Role.Media);
         User newUser = userService.saveUser(user);
         return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
@@ -46,9 +50,24 @@ public class UserController {
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if (result.hasErrors()) return null;
 
+        user.setStatus(false);
         user.setRole(Role.User);
         User newUser = userService.saveUser(user);
         return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
 
+    }
+
+    @PutMapping("/update/{user_id}")
+    public User updateUser(@PathVariable(value = "user_id") Long user_id,
+                              @Valid @RequestBody User user) {
+
+        User u = repository.findById(user_id)
+                .orElseThrow(() -> new ResourceNotFoundException("User"+ "user_id"+ user_id));
+
+        u.setAbout(user.getAbout());
+        u.setPhone(user.getPhone());
+
+        User updatedUser = repository.save(u);
+        return updatedUser;
     }
 }
